2a4a0e587921ca3a1c23dad29d21e26bfde83abc
Siarhei Beliakou <sbeliakou@gmail.com>
2019-10-01 23:46:32 +0300

    cluster-init

--- k8s-practice-course/k8s-pods/cluster-init.sh
+++ k8s-practice-course/k8s-pods/cluster-init.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-kubeadm init
+kubeadm init --token abcdef.0123456789abcdef --token-ttl 0
+
+API=$(hostname -I | cut -d' ' -f1)
+ssh node01 "kubeadm join --token abcdef.0123456789abcdef --discovery-token-unsafe-skip-ca-verification $(API):6443"
+
+mkdir ~/.kube
+cp /etc/kubernetes/admin.conf ~/.kube/config
+kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
--- k8s-practice-course/k8s-pods/set-env.sh
+++ k8s-practice-course/k8s-pods/set-env.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
 
-while $(ps -ef | grep [k]ubeadm >/dev/null); do echo alive; sleep 1; done
+while $(kubectl get componentstatus >/dev/null); do echo -n .; sleep 1; done; echo
