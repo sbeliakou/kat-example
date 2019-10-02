9431dd5328d70c073328966647600e50c3f6791e
Siarhei Beliakou <sbeliakou@gmail.com>
2019-10-02 10:53:31 +0300

    upd

--- k8s-practice-course/k8s-pods/step2-verify.sh
+++ k8s-practice-course/k8s-pods/step2-verify.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+[ `kubectl get po multi-pod -o jsonpath='{.status.phase}'` == "Running" ] &&
+[ `kubectl get po multi-pod -o jsonpath='{.spec.containers[0].name}'` == "alpha" ] &&
+[ `kubectl get po multi-pod -o jsonpath='{.spec.containers[0].image}'` == "nginx:alpine" ] &&
+[ `kubectl get po multi-pod -o jsonpath='{.spec.containers[0].env[?(@.name=="type")].value}'` == "main" ] &&
+[ `kubectl get po multi-pod -o jsonpath='{.spec.containers[1].name}'` == "beta" ] &&
+[ `kubectl get po multi-pod -o jsonpath='{.spec.containers[1].image}'` == "busybox" ] &&
+[ `kubectl get po multi-pod -o jsonpath='{.spec.containers[1].env[?(@.name=="type")].value}'` == "sidecar" ] &&
+[ "`kubectl get po multi-pod -o jsonpath='{.spec.containers[1].command}'`" == "[sleep 4800]" ] &&
 echo done
--- k8s-practice-course/k8s-pods/step2.md
+++ k8s-practice-course/k8s-pods/step2.md
@@ -1,12 +1,12 @@
 Create a pod called `multi-pod` with two containers. 
 
 **Container 1**:
-- name: alpha
-- image: nginx
+- name: `alpha`
+- image: `nginx:alpine`
 - environment variables: type=main
 
 **Container 2**:
-- name: beta
-- image: busybox
+- name: `beta`
+- image: `busybox`
 - environment variables: type=sidecar
-- command: sleep 4800
+- command: `sleep 4800`
