8f433682a2fe170f344bada3ed62f8c4241dc982
Siarhei Beliakou <sbeliakou@gmail.com>
2019-10-03 23:57:43 +0300

    upd

---
+++ k8s-practice-course/k8s-init/askname.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo Hello, who am I talking to?
+
+echo FirstName:
+read firstname
+
+echo LastName:
+read lastname
+
+echo email:
+read email
+
+echo "Hello ${firstname} ${lastname}"
+
--- k8s-practice-course/k8s-init/step1-verify.sh
+++ k8s-practice-course/k8s-init/step1-verify.sh
@@ -6,3 +6,6 @@ kubectl get nodes >/dev/null 2>&1 &&
 kubeadm token list | grep abcdef.0123456789abcdef >/dev/null &&
 [ `kubeadm token list | grep abcdef.0123456789abcdef | awk '{print $2}' | sed 's/[^0-9]//g'` -le 20 ] &&
 echo done
+
+
+# kubectl get secrets -n kube-system bootstrap-token-abcdef -o jsonpath='{.data.expiration}' | base64 -d | xargs -IF echo $(( (`date -d 'F' +%s` - `date +%s`)/60 ))
