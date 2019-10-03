5d8c92ea17d220d8a76dd33b94c6cb853d88383c
Siarhei Beliakou <sbeliakou@gmail.com>
2019-10-03 21:14:27 +0300

    upd

--- k8s-practice-course/k8s-init/step1-verify.sh
+++ k8s-practice-course/k8s-init/step1-verify.sh
@@ -2,5 +2,6 @@
 
 export KUBECONFIG=/etc/kubernetes/admin.conf
 
-kubectl get nodes >/dev/null 2>&1 && 
+kubectl get nodes >/dev/null 2>&1 &&
+kubeadm token list | grep abcdef.0123456789abcdef >/dev/null &&
 echo done
--- k8s-practice-course/k8s-init/step3-verify.sh
+++ k8s-practice-course/k8s-init/step3-verify.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+kubectl get pods -n kube-system | grep weave | grep Running >/dev/null &&
 echo done
