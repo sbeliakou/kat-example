d19ac2ca50382600b97523af3d47a90abdcea39c
Aleksandra Kuznetsova <alexminsk.noir@gmail.com>
2019-10-07 13:31:56 +0300

    Edited task 3-1

--- k8s-practice-course/k8s-deployments/step1-verify.sh
+++ k8s-practice-course/k8s-deployments/step1-verify.sh
@@ -2,7 +2,7 @@
 
 [ `kubectl get deployment nginx-deploy -o jsonpath='{.metadata.name}'` == "nginx-deploy" ] && 
 [ `kubectl get deployment nginx-deploy -o jsonpath='{.spec.template.spec.containers[0].image}'` == "nginx:1.16-alpine" ] && 
-[ `kubectl get deployment nginx-deploy -o jsonpath='{.spec.replicas.}'` == 1 ] &&
+[ `kubectl get deployment nginx-deploy -o jsonpath='{.spec.replicas}'` == 1 ] &&
 [ `kubectl get deployment nginx-deploy -o jsonpath='{.status.readyReplicas}'` == 1 ] &&
 echo done 
 
