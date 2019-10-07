c2a67653f66469a644e177d911b28b3883a9110f
Aleksandra Kuznetsova <alexminsk.noir@gmail.com>
2019-10-07 14:58:07 +0300

    Edited 3-1

--- k8s-practice-course/k8s-deployments/step2-verify.sh
+++ k8s-practice-course/k8s-deployments/step2-verify.sh
@@ -4,5 +4,7 @@
 [ `kubectl get deployment orange -n orange -o jsonpath='{.spec.template.spec.containers[0].image}'` == "nginx" ] && 
 [ `kubectl get deployment orange -n orange -o jsonpath='{.spec.replicas}'` == 2 ] &&
 [ `kubectl get deployment orange -n orange -o jsonpath='{.status.readyReplicas}'` == 2 ] &&
+[ `kubectl get deployment orange -n orange -o jsonpath='{.spec.template.spec.initContainers[0].image}'` == "busybox:latest" ] &&
+[ `kubectl get deployment orange -n orange -o jsonpath='{.spec.template.spec.initContainers[0].command[1]}'` == 10  ]
 echo done 
 
