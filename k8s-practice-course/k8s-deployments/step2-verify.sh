bad47622732fecabbf213294136db4c4cf16b6f5
Aleksandra Kuznetsova <alexminsk.noir@gmail.com>
2019-10-07 18:06:53 +0300

    Added 3-3

--- k8s-practice-course/k8s-deployments/step2-verify.sh
+++ k8s-practice-course/k8s-deployments/step2-verify.sh
@@ -2,6 +2,6 @@
 
 [ `kubectl get deployment nginx-deploy -o jsonpath='{.spec.template.spec.containers[0].image}'` == "nginx:1.17-alpine" ] && 
 [ `kubectl rollout history deployment nginx-deploy | awk '{print $7}' | tail -3 | head -1` == "nginx=nginx:1.16-alpine" ] && 
-[  `kubectl rollout history deployment nginx-deploy | awk '{print $7}' | tail -3 | head -1` == "nginx=nginx:1.17-alpine" ] &&
+[  `kubectl rollout history deployment nginx-deploy | awk '{print $7}' | tail -2 | head -1` == "nginx=nginx:1.17-alpine" ] &&
 echo done 
 
