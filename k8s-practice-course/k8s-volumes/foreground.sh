a2f6df8b6f68f4398b0392ff5dbcfdc93cb5994c
Aleksandra Kuznetsova <alexminsk.noir@gmail.com>
2019-10-08 12:09:44 +0300

    Added 4

--- k8s-practice-course/k8s-volumes/foreground.sh
+++ k8s-practice-course/k8s-volumes/foreground.sh
@@ -1,3 +1,6 @@
 #!/bin/bash 
 
-clear && echo -n "Prepairing Environment " && until $(kubectl get componentstatus >/dev/null 2>&1); do echo -n .; sleep 1; done; echo; history -c
+clear && echo -n "Prepairing kubelet" && 
+	until $(kubelet --version >/dev/null 2>&1); do echo -n .; sleep 1; done; echo "\nWaiting for master to join" &&
+	until $(kubectl get componentstatus > /dev/null 2>&1); do echo -n .; sleep 1; done; echo "\nWaiting for master to get ready" &&
+	until $(kubectl get nodes | grep NotReady > /dev/null 2>&1); do echo -n .; sleep 1; done; echo; history -c; clear; echo "Time to rock!"
--- k8s-practice-course/k8s-volumes/step1.md
+++ k8s-practice-course/k8s-volumes/step1.md
@@ -1,1 +1,8 @@
-Create 
+Create 30 Mi persistent volume `pv-first`. Create claim `pvc-first` which will bound this volume. Attach created volume to pod `nginx-pod` with image `nginx:mainline-perl`.
+
+Requirements:
+- pod `nginx-pod` uses proper image
+- volume `pv-first` has right properties
+- volume `pv-first` is mounted to `nginx-pod` 
+- volume `pv-first` uses host path `/opt/data1`
+- volume `pv-first` has reclaim policy `Reatin` and access mode `ReadWriteOnce`
