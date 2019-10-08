f0c79328ea66ba1c546a1c42d3c37915f975f4fd
Aleksandra Kuznetsova <alexminsk.noir@gmail.com>
2019-10-08 12:14:04 +0300

    Added 4

--- k8s-practice-course/k8s-volumes/foreground.sh
+++ k8s-practice-course/k8s-volumes/foreground.sh
@@ -3,4 +3,4 @@
 clear && echo -n "Prepairing kubelet" && 
 	until $(kubelet --version >/dev/null 2>&1); do echo -n .; sleep 1; done; echo "\nWaiting for master to join" &&
 	until $(kubectl get componentstatus > /dev/null 2>&1); do echo -n .; sleep 1; done; echo "\nWaiting for master to get ready" &&
-	until $(kubectl get nodes | grep NotReady > /dev/null 2>&1); do echo -n .; sleep 1; done; echo; history -c; clear; echo "Time to rock!"
+	until $(kubectl get nodes | grep Ready > /dev/null 2>&1); do echo -n .; sleep 1; done; echo; history -c; clear; echo "Time to rock!"
