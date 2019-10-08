860c30036985e4fd7a36dd12e1faf9835c70115d
Aleksandra Kuznetsova <alexminsk.noir@gmail.com>
2019-10-08 09:58:35 +0300

    Edited 3-2

--- k8s-practice-course/k8s-deployments/step2-verify.sh
+++ k8s-practice-course/k8s-deployments/step2-verify.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 [ `kubectl get deployment nginx-deploy -o jsonpath='{.spec.template.spec.containers[0].image}'` == "nginx:1.16-alpine" ] && 
-[ `kubectl rollout history deployment nginx-deploy | grep -c "nginx=nginx:1.16-alpine"` >= 1 ] && 
-[ `kubectl rollout history deployment nginx-deploy | grep -c "nginx=nginx:1.17-alpine"` >= 1 ] &&
-[ `kubectl rollout history deployment nginx-deploy | grep -c "set image"` >= 2 ] &&
+[ `kubectl rollout history deployment nginx-deploy | grep -c "nginx=nginx:1.16-alpine"` -ge 1 ] && 
+[ `kubectl rollout history deployment nginx-deploy | grep -c "nginx=nginx:1.17-alpine"` -ge 1 ] &&
+[ `kubectl rollout history deployment nginx-deploy | grep -c "set image"` -ge 2 ] &&
 echo done 
 
