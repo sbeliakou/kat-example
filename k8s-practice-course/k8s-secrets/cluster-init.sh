1af2df306493358cd4871899861fa065e162c2ef
Aliaksandr Dalimaeyu <aliaksandr_dalimayeu@epam.com>
2019-10-08 06:38:33 -0400

    secrets

--- k8s-practice-course/k8s-secrets/cluster-init.sh
+++ k8s-practice-course/k8s-secrets/cluster-init.sh
@@ -7,3 +7,6 @@ cp /etc/kubernetes/admin.conf ~/.kube/config
 
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 ssh -o StrictHostKeyChecking=no node01 "kubeadm join --token abcdef.0123456789abcdef --discovery-token-unsafe-skip-ca-verification $(hostname -I | cut -d' ' -f1):6443"
+
+kubectl taint nodes --all node-role.kubernetes.io/master-
+
