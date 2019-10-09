f7f498769477ed08a1d7c4cda473ab81263089fb
Aliaksandr Dalimaeyu <aliaksandr_dalimayeu@epam.com>
2019-10-09 07:06:47 -0400

    services

--- k8s-practice-course/k8s-services/foreground-step7-preparing.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-clear && echo -n "Prepairing Environment " && until [[ $(kubectl get deployments trouble-dep | awk 'FNR==2 {print $2}') == '3/3' ]]; do echo -n .; sleep 1; done; echo; history -c
-
-
