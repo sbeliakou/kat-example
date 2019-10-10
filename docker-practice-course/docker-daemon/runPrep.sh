406bbdabe6c5a77ae1d9d06c35544dff79e9d7ef
Siarhei Beliakou <sbeliakou@gmail.com>
2019-10-10 11:51:41 +0300

    docker

--- docker-practice-course/docker-daemon/runPrep.sh
+++ docker-practice-course/docker-daemon/runPrep.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 :> /opt/launch-kubeadm.sh
-ps -ef | grep /opt/launch-kubeadm.sh | awk '{print $2}' | xargs -r kill -9
+ps -ef | grep /opt/launch-kubeadm.sh | grep -v grep | awk '{print $2}' | xargs -r kill -9
 kubeadm reset -f || true
+docker image ls | grep k8s.gcr.io | awk '{print $3}' | xargs -r docker image rm
 
 DIR=$(mktemp -d -p /var/tmp)
-
 wget -O ${DIR}/main https://github.com/sbeliakou/kat-example/raw/master/assets/main
 wget -O ${DIR}/form.html https://github.com/sbeliakou/kat-example/raw/master/assets/form.html
 chmod a+x ${DIR}/main
