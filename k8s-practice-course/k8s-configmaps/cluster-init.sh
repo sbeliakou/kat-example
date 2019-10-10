bf08f28286066950f5d8ec0b5b732cfc3207bf41
Aliaksandr Dalimaeyu <aliaksandr_dalimayeu@epam.com>
2019-10-10 03:01:23 -0400

    configmaps

--- k8s-practice-course/k8s-configmaps/cluster-init.sh
+++ k8s-practice-course/k8s-configmaps/cluster-init.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-kubeadm init --token abcdef.0123456789abcdef --token-ttl 0
+bash /opt/launch-kubeadm.sh
 
-mkdir ~/.kube
-cp /etc/kubernetes/admin.conf ~/.kube/config
 
-kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
-ssh -o StrictHostKeyChecking=no node01 "kubeadm join --token abcdef.0123456789abcdef --discovery-token-unsafe-skip-ca-verification $(hostname -I | cut -d' ' -f1):6443"
+#kubeadm init --token abcdef.0123456789abcdef --token-ttl 0
 
-kubectl taint nodes --all node-role.kubernetes.io/master-
+#mkdir ~/.kube
+#cp /etc/kubernetes/admin.conf ~/.kube/config
+
+#kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+#ssh -o StrictHostKeyChecking=no node01 "kubeadm join --token abcdef.0123456789abcdef --discovery-token-unsafe-skip-ca-verification $(hostname -I | cut -d' ' -f1):6443"
+
+#kubectl taint nodes --all node-role.kubernetes.io/master-
 
