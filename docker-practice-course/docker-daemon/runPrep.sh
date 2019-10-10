93ebc34f8c42bf1e4727cb18c9a9c438e7cfb95f
Siarhei Beliakou <sbeliakou@gmail.com>
2019-10-10 11:04:40 +0300

    docker

--- docker-practice-course/docker-daemon/runPrep.sh
+++ docker-practice-course/docker-daemon/runPrep.sh
@@ -6,7 +6,5 @@ wget -O ${DIR}/main https://github.com/sbeliakou/kat-example/raw/master/assets/m
 wget -O ${DIR}/form.html https://github.com/sbeliakou/kat-example/raw/master/assets/form.html
 chmod a+x ${DIR}/main
 
-wget -O /lib/systemd/system/main.service https://github.com/sbeliakou/kat-example/raw/master/assets/main.service
-sed -i "s@TMPDIR@${DIR}@" /lib/systemd/system/main.service
-systemctl enable main.service
-systemctl start main.service
+cd ${DIR}/
+nohup ./main &
