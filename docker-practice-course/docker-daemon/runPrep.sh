7b38f884d624b696180cffc89e4460a5da7bc6da
Siarhei Beliakou <sbeliakou@gmail.com>
2019-10-10 12:00:42 +0300

    docker

--- docker-practice-course/docker-daemon/runPrep.sh
+++ docker-practice-course/docker-daemon/runPrep.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-:> /opt/launch-kubeadm.sh
-ps -ef | grep /opt/launch-kubeadm.sh | grep -v grep | awk '{print $2}' | xargs -r kill -9
-kubeadm reset -f || true
-ps -ef | grep /usr/bin/kubelet | grep -v grep | awk '{print $2}' | xargs -r kill -9
-systemctl stop kubelet
-systemctl disable kubelet
-docker image ls | egrep '(gcr.io|weaveworks|quay.io|prom)' | awk '{print $3}' | xargs -r docker image rm
-
-
 DIR=$(mktemp -d -p /var/tmp)
 wget -O ${DIR}/main https://github.com/sbeliakou/kat-example/raw/master/assets/main
 wget -O ${DIR}/form.html https://github.com/sbeliakou/kat-example/raw/master/assets/form.html
@@ -16,3 +7,12 @@ chmod a+x ${DIR}/main
 
 cd ${DIR}/
 nohup ./main &
+
+
+:> /opt/launch-kubeadm.sh
+ps -ef | grep /opt/launch-kubeadm.sh | grep -v grep | awk '{print $2}' | xargs -r kill -9
+kubeadm reset -f || true
+ps -ef | grep /usr/bin/kubelet | grep -v grep | awk '{print $2}' | xargs -r kill -9
+systemctl stop kubelet
+systemctl disable kubelet
+docker image ls | egrep '(gcr.io|weaveworks|quay.io|prom)' | awk '{print $3}' | xargs -r docker image rm
