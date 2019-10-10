06ddd0d9e3278413614364e49316f2a780b41c57
Siarhei Beliakou <sbeliakou@gmail.com>
2019-10-10 11:47:17 +0300

    docker

--- docker-practice-course/docker-daemon/runPrep.sh
+++ docker-practice-course/docker-daemon/runPrep.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+:> /opt/launch-kubeadm.sh
+ps -ef | grep /opt/launch-kubeadm.sh | awk '{print $2}' | xargs -r kill -9
+kubeadm reset -f || true
+
 DIR=$(mktemp -d -p /var/tmp)
 
 wget -O ${DIR}/main https://github.com/sbeliakou/kat-example/raw/master/assets/main
