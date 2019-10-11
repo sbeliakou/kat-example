0e0ad57cc8c215d4dd690abb8cd76adf5ad55dec
Aliaksandr Dalimaeyu <aliaksandr_dalimayeu@epam.com>
2019-10-11 11:40:54 -0400

    upd

--- k8spok-practice-course/k8spok-services/step1-courseData.sh
+++ k8spok-practice-course/k8spok-services/step1-courseData.sh
@@ -2,3 +2,48 @@
 
 kubectl create ns red
 
+cat << EOF | kubectl apply -f-
+apiVersion: v1
+kind: Service
+metadata:
+  name: red-cluster-svc
+  namespace: red
+spec:
+  type: ClusterIP
+  selector:
+    app: red-pod
+  ports:
+    - port: 80
+EOF
+
+cat << EOF | kubectl apply -f-
+apiVersion: v1
+kind: Service
+metadata:
+  name: red-node-svc
+  namespace: red
+spec:
+  type: NodePort
+  selector:
+    app: red-pod
+  ports:
+    - port: 8080
+      targetPort: 80
+      nodePort: 31200
+EOF
+
+cat << EOF | kubectl apply -f-
+apiVersion: v1
+kind: Service
+metadata:
+  name: red-lb-svc
+  namespace: red
+spec:
+  type: LoadBalancer
+  selector:
+    app: red-pod
+  ports:
+    - port: 80
+      targetPort: 8000
+EOF
+
