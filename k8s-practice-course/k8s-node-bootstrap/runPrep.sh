43ba38bb877cffa97097a5deb0f79887b53de12c
Siarhei Beliakou <sbeliakou@gmail.com>
2019-10-12 21:18:59 +0300

    upd

--- k8s-practice-course/k8s-node-bootstrap/runPrep.sh
+++ k8s-practice-course/k8s-node-bootstrap/runPrep.sh
@@ -4,5 +4,5 @@
 
 ssh -o StrictHostKeyChecking=no node01 "mkdir -p /etc/kubernetes"
 ssh -o StrictHostKeyChecking=no node01 'echo KUBELET_KUBECONFIG_ARGS="--bootstrap-kubeconfig=/etc/kubernetes/bootstrap-kubelet.conf --kubeconfig=/etc/kubernetes/kubelet.conf --cgroup-driver=systemd"> /etc/default/kubelet'
-ssh -o StrictHostKeyChecking=no node01 'echo KUBELET_CONFIG_ARGS=""> /etc/default/kubelet'
+ssh -o StrictHostKeyChecking=no node01 'echo KUBELET_CONFIG_ARGS="">> /etc/default/kubelet'
 ssh -o StrictHostKeyChecking=no node01 'systemctl daemon-reload'
