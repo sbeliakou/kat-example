d16cee23d8b300828ccb10d043a4b051cad324a0
Siarhei Beliakou <sbeliakou@gmail.com>
2019-10-12 23:43:34 +0300

    upd

--- k8s-practice-course/k8s-node-bootstrap/finish.md
+++ k8s-practice-course/k8s-node-bootstrap/finish.md
@@ -1,13 +1,1 @@
 # It's Done!
-
-Some useful commands to know:
-
-<pre class="file">
-
-kubectl run web --image=httpd --generator=run-pod/v1
-kubectl run web --image=httpd --generator=run-pod/v1 --dry-run -o yaml
-kubectl label pod app=web
-
-kubectl describe pod web
-kubectl get pod web -o yaml
-</pre>
--- k8s-practice-course/k8s-node-bootstrap/step2-verify.sh
+++ k8s-practice-course/k8s-node-bootstrap/step2-verify.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-[ `kubectl get po test -o jsonpath='{.status.phase}'` == "Running" ] &&
-[ `kubectl get po test -o jsonpath='{.spec.containers[0].image}'` == "alpine:latest" ] &&
-[ "`kubectl get po test -o jsonpath='{.spec.containers[1].command}'`" == "[sleep 1000]" ] &&
+[[ `kubectl get po test -o jsonpath='{.status.phase}'` == "Running" ]] &&
+[[ `kubectl get no node01 -o jsonpath='{.status.addresses[?(@.type=="InternalIP")].address}'` == `kubectl get nodes node01 -o jsonpath='{.status.addresses[?(@.type=="InternalIP")].address}'` ]] &&
+[[ "`kubectl get po test -o jsonpath='{.spec.containers[0].image}'`" == "alpine:latest" ]] &&
+[[ "`kubectl get po test -o jsonpath='{.spec.containers[0].command}'`" == "[sleep 1000]" ]] &&
 echo done
