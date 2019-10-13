e20ab4bf729f1e79d70ba780bd7d190a0382e8be
Aliaksandr Dalimayeu <51114405+adalimaev@users.noreply.github.com>
2019-10-13 17:14:57 +0300

    Update step1-courseData.sh

--- k8spok-practice-course/k8s-volumes/step1-courseData.sh
+++ k8spok-practice-course/k8s-volumes/step1-courseData.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
-        
 for i in {1..5}; do
-  mkdir -p /opt/data${i} &&
-  cat << EOF | kubectl apply -f-
-  apiVersion: v1
-  kind: PersistentVolume
-  metadata:
-    name: pv-${i}
-    labels:
-      type: local
-  spec:
-    storageClassName: manual
-    capacity:
-      storage: 10Mi
-    accessModes:
-      - ReadWriteOnce
-    hostPath:
-      path: "/opt/data${i}"
-  EOF
+mkdir -p /opt/data${i}
+cat << EOF | kubectl apply -f-
+apiVersion: v1
+kind: PersistentVolume
+metadata:
+  name: pv-${i}
+  labels:
+    type: local
+spec:
+  storageClassName: manual
+  capacity:
+    storage: 10Mi
+  accessModes:
+    - ReadWriteOnce
+  hostPath:
+    path: "/opt/data${i}"
+EOF
 done
 
