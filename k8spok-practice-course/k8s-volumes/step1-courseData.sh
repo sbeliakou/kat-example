2b6a11f4aea1dd105ec02f8cd81937f8e6a63bab
Aliaksandr Dalimayeu <devops@localhost.localdomain>
2019-10-13 10:10:09 -0400

    k8spok upd

--- k8spok-practice-course/k8s-volumes/step1-courseData.sh
+++ k8spok-practice-course/k8s-volumes/step1-courseData.sh
@@ -1,81 +1,23 @@
 #!/bin/bash
 
         
-mkdir -p /opt/data{1..5} &&
-cat << EOF | kubectl apply -f-
-apiVersion: v1
-kind: PersistentVolume
-metadata:
-  name: pv-first
-  labels:
-    type: local
-spec:
-  storageClassName: manual
-  capacity:
-    storage: 10Mi
-  accessModes:
-    - ReadWriteOnce
-  hostPath:
-    path: "/opt/data1"
----
-apiVersion: v1
-kind: PersistentVolume
-metadata:
-  name: pv-second
-  labels:
-    type: local
-spec:
-  storageClassName: manual
-  capacity:
-    storage: 10Mi
-  accessModes:
-    - ReadWriteOnce
-  hostPath:
-    path: "/opt/data2"
----
-apiVersion: v1
-kind: PersistentVolume
-metadata:
-  name: pv-third
-  labels:
-    type: local
-spec:
-  storageClassName: manual
-  capacity:
-    storage: 10Mi
-  accessModes:
-    - ReadWriteOnce
-  hostPath:
-    path: "/opt/data3"
----
-apiVersion: v1
-kind: PersistentVolume
-metadata:
-  name: pv-fourth
-  labels:
-    type: local
-spec:
-  storageClassName: manual
-  capacity:
-    storage: 10Mi
-  accessModes:
-    - ReadWriteOnce
-  hostPath:
-    path: "/opt/data4"
----
-apiVersion: v1
-kind: PersistentVolume
-metadata:
-  name: pv-fifth
-  labels:
-    type: local
-spec:
-  storageClassName: manual
-  capacity:
-    storage: 10Mi
-  accessModes:
-    - ReadWriteOnce
-  hostPath:
-    path: "/opt/data5"
-EOF
+for i in {1..5}; do
+  mkdir -p /opt/data${i} &&
+  cat << EOF | kubectl apply -f-
+  apiVersion: v1
+  kind: PersistentVolume
+  metadata:
+    name: pv-${i}
+    labels:
+      type: local
+  spec:
+    storageClassName: manual
+    capacity:
+      storage: 10Mi
+    accessModes:
+      - ReadWriteOnce
+    hostPath:
+      path: "/opt/data${i}"
+  EOF
+done
 
