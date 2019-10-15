4dcb0d3f76a3400e17505064153cae362298cee2
Aliaksandr Dalimaeyu <aliaksandr_dalimayeu@epam.com>
2019-10-15 08:16:45 -0400

    docker images upd

---
+++ docker-practice-course/docker-images/step6-verify.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+[ -f /.ok ] && echo done ||
+[[ $(docker run mybusybox pwd 2>&1) == '/data']] &&
+$(docker run mybusybox ls test_file1 >/dev/null 2>&1) &&
+$(docker run mybusybox ls test_file2 >/dev/null 2>&1) &&
+[[ $(docker run mybusybox env 2>&1 | grep -c MAINTAINER) -ge 1 ]]  &&
+echo done || exit 0
+
+TASK_SCORE="1"
+source /opt/.user
+cat << EOF | curl -s -X POST --data @- https://s9cfrymdt8.execute-api.eu-west-1.amazonaws.com/default/lab_logger >/dev/null 2>&1
+{
+  "table": "docker",
+  "timestamp": "$(date --iso-8601=ns --utc)",
+  "payload": {
+    "name": "${FIRSTNAME} ${LASTNAME}",
+    "email": "${EMAIL}",
+    "scenario": "docker-images.6", 
+    "score": ${TASK_SCORE}
+  }
+}
+EOF
+
+
---
+++ docker-practice-course/docker-images/step6.md
@@ -0,0 +1,11 @@
+
+You're given **test_file1** file and **test_arch.tar.gz** archive in `/root/` directory.  
+Create Dockerfile and build image with the following requirements.  
+
+## Requirements:
+- image tag: **mybusybox**;
+- base image: **busybox**;
+- work directory: **/data**;
+- **test_file1** file should be copied to work directory;
+- content of **test_arch.tar.gz** archive should be copied to work directory;
+- image should have environment variable: `MAINTAINER=student`
