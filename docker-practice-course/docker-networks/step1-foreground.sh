1b8c9f840941eb87409ea362c57a5572278fccc0
Aliaksandr Dalimaeyu <aliaksandr_dalimayeu@epam.com>
2019-10-15 03:33:12 -0400

    upd

--- docker-practice-course/docker-networks/step1-foreground.sh
+++ docker-practice-course/docker-networks/step1-foreground.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+clear && echo -n "Prepairing Environment " && until $(docker image ls | egrep '(gcr.io|weaveworks|quay.io|prom|katacoda)' >/dev/null 2>&1); do echo -n .; sleep 1; done; echo; history -c
 
