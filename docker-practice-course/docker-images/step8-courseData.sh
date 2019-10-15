35b0eb560d3f9e1436e3525b651a303a56d0620c
Aliaksandr Dalimayeu <devops@localhost.localdomain>
2019-10-15 10:17:39 -0400

    docker_upd

--- docker-practice-course/docker-images/step8-courseData.sh
+++ docker-practice-course/docker-images/step8-courseData.sh
@@ -6,7 +6,7 @@
 ##### preparing for DOCKER ####
 
 
-mkdir -p /data/maven
+mkdir -p /data/maven && cd /data/maven
 cat << 'EOF' > /data/maven/Dockerfile
 FROM maven:3.6-jdk-8-alpine
 WORKDIR /app
@@ -15,6 +15,6 @@ RUN mvn -e -B dependency:resolve
 COPY src ./src
 RUN mvn -e -B package
 EOF
-docker run -ti --rm -v ${HOME}:/root -v /data/maven:/git alpine/git clone https://github.com/jabedhasan21/java-hello-world-with-maven.git
-cp -r /data/maven/java-hello-world-with-maven/* /data/maven/
+docker run -ti --rm -v ${HOME}:/root -v $(pwd):/git alpine/git clone https://github.com/jabedhasan21/java-hello-world-with-maven.git
+cp -r java-hello-world-with-maven/* .
 
