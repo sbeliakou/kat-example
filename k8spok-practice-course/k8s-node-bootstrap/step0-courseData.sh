8bf23d06b26a825a599598288164465dae3751fc
Aliaksandr Dalimaeyu <aliaksandr_dalimayeu@epam.com>
2019-10-16 11:16:26 -0400

    upd

--- k8spok-practice-course/k8s-node-bootstrap/step0-courseData.sh
+++ k8spok-practice-course/k8s-node-bootstrap/step0-courseData.sh
@@ -12,4 +12,16 @@ kubectl taint nodes --all node-role.kubernetes.io/master-
 ##### preparing for DOCKER ####
 
 
+ping -c1 node01 || ( echo "Something went wrong, sorry" && shutdown -r now )
+
+#/opt/launch-kubeadm.sh
+kubectl delete configmap -n kube-public cluster-info
+kubectl delete secret -n kube-system bootstrap-token-96771a
+
+ssh -o StrictHostKeyChecking=no node01 "mkdir -p /etc/kubernetes"
+# ssh -o StrictHostKeyChecking=no node01 'echo KUBELET_KUBECONFIG_ARGS=\"--bootstrap-kubeconfig=/etc/kubernetes/bootstrap-kubelet.conf --kubeconfig=/etc/kubernetes/kubelet.conf --cgroup-driver=systemd\"> /etc/default/kubelet'
+# ssh -o StrictHostKeyChecking=no node01 'echo KUBELET_CONFIG_ARGS=\"\">> /etc/default/kubelet'
+# ssh -o StrictHostKeyChecking=no node01 'systemctl daemon-reload'
+
+ssh -o StrictHostKeyChecking=no node01 "rm -rf /etc/systemd/system/kubelet.service.d"
 
